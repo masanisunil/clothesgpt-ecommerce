d91babe04213861f1d9b0e65706af308f1017ffd
masanisunil <sunil.m0711@gmail.com>
2026-01-12 16:33:07 +0530

    first_commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
+// Attach token automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
